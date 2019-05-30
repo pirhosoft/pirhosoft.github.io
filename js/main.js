8417c2f42b724def4c1884d5bb72818dc059dc39
Luke Larson <luke.larson@gmail.com>
2019-05-30 11:54:07 -0700

    history blog

--- js/main.js
+++ js/main.js
@@ -23,6 +23,7 @@ var pages =
 
 var posts =
 {
+	"2019-05-30": [ "History Window Using UIElements", "A simple selection history window created to learn Unity's new UIElements package" ],
 	"2019-05-27": [ "Yielding Coroutines Without Delay", "Eliminate the frame delay introduced by yielding an iterator method in a coroutine" ],
 	"2018-03-19": [ "The Art of War Released!", "We have officially released the 1.0 version of The Art of War on Windows and Xbox" ],
 	"2017-10-10": [ "PiRho Soft and The Art of War", "Introducing our independent development studio PiRho Soft and our first game: The Art of War" ]
